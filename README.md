9d22a927e95c0b71b56a743388946f35d3515fd0
Code Ass <aycabta@gmail.com>
2015-12-31 03:16:31 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Lingr Presence Details
+======================
+
+[This Greasemonkey script on OpenUserJS.org](https://openuserjs.org/scripts/aycabta/Lingr_Presence_Details)
+
+## Example
+
+![icon](chat-people.png)
+
+ICON!!!!!!!!!!!!!
+
---
+++ lingr-presence-details.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name       Lingr presence details
+// @namespace  http://aycabta.github.io/
+// @version    0.0.1
+// @description  The Benry Script
+// @include    http://lingr.com/
+// @copyright  2015+, Code Ass
+// ==/UserScript==
+
+(function() {
+    RosterView.prototype.oldLingrPresenceDetailsChangePresence = RosterView.prototype.changePresence;
+    RosterView.prototype.changePresence = function(event) {
+        event.text = '<img src="' + event.presence.icon_url + '" alt="' + event.presence.nickname + '" width="16" height="16" />' + event.text;
+        return RosterView.prototype.oldLingrPresenceDetailsChangePresence.call(this, event);
+    }
+    Message.prototype.oldLingrPresenceDetailsInitialize = Message.prototype.initialize;
+    Message.prototype.initialize = function(json) {
+        var result = Message.prototype.oldLingrPresenceDetailsInitialize.call(this, json);
+        if (json.type === 'system') {
+            result.attributes.decorated_text = result.attributes.text;
+        }
+        return result;
+    };
+})();
